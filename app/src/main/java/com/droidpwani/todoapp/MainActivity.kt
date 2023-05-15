c0d269e02c4faec13bc6aa1270992791df109b8c
Beatrice Kinya <gitongakinya1@gmail.com>
2023-05-15 10:19:33 +0300

    imports

--- app/src/main/java/com/droidpwani/todoapp/MainActivity.kt
+++ app/src/main/java/com/droidpwani/todoapp/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.droidpwani.todoapp
 
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.droidpwani.todoapp.ui.screens.CounterScreen
 import com.droidpwani.todoapp.ui.theme.TodoAppTheme
